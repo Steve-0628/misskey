f2a846d02bcb70d4832264e7700f6280ff3931bb
Steve-0628 <me@stesan.dev>
2025-03-27 01:53:06 +0900

    fix

--- packages/backend/migration/1743007186939-drop-page.js
+++ packages/backend/migration/1743007186939-drop-page.js
@@ -1,8 +1,9 @@
 export class DropPage1743007186939 {
 
     async up(queryRunner) {
-			queryRunner.query(`DROP TABLE IF EXISTS "page";`);
+			queryRunner.query(`ALTER TABLE "user_profile" DROP COLUMN "pinnedPageId";`);
 			queryRunner.query(`DROP TABLE IF EXISTS "page_like";`);
+			queryRunner.query(`DROP TABLE IF EXISTS "page";`);
     }
 
     async down(queryRunner) {
