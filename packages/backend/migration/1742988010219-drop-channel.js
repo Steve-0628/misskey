5796d2ce9be78c764b3291b8fe434fe69db40803
Steve-0628 <me@stesan.dev>
2025-03-28 18:49:46 +0900

    fix: migration typo

--- packages/backend/migration/1742988010219-drop-channel.js
+++ packages/backend/migration/1742988010219-drop-channel.js
@@ -5,7 +5,7 @@ export class DropChannel1742988010219 {
 			 await queryRunner.query(`ALTER TABLE "note" DROP COLUMN "channelId";`);
 			 await queryRunner.query(`ALTER TABLE "note_unread" DROP COLUMN "noteChannelId";`);
 			 await queryRunner.query(`DROP TABLE IF EXISTS "channel" CASCADE;`);
-			 await queryRunner.query(`DROP TABLE IF EXISTS "channel_note_pinning";`);
+			 await queryRunner.query(`DROP TABLE IF EXISTS "channel_note_pining";`);
 			 await queryRunner.query(`DROP TABLE IF EXISTS "channel_favorite";`);
 			 await queryRunner.query(`DROP TABLE IF EXISTS "channel_following";`);
     }
