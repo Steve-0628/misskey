a09990ae08d825be61976a4365c9ec8385bbcd42
Steve-0628 <49326405+Steve-0628@users.noreply.github.com>
2025-11-18 17:38:06 +0000

    remove storybook stuff

--- packages/frontend/.storybook/preload-locale.ts
+++
@@ -1,8 +0,0 @@
-import { writeFile } from 'node:fs/promises';
-import locales from '../../../locales/index.js';
-
-await writeFile(
-	new URL('locale.ts', import.meta.url),
-	`export default ${JSON.stringify(locales['ja-JP'], undefined, 2)} as const;`,
-	'utf8',
-)
